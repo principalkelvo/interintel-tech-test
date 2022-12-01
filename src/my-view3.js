bab531bbe85a83d20a8ae6a6564401322e5f0f02
principalkelvo <principalkelvo@gmail.com>
2022-12-01 20:35:29 +0300

    Changes view-3 to lit-element console logs fetched data

--- src/my-view3.js
+++ src/my-view3.js
@@ -8,19 +8,55 @@
  * subject to an additional IP rights grant found at http://polymer.github.io/PATENTS.txt
  */
 
-import { PolymerElement, html } from '@polymer/polymer/polymer-element.js';
+// import { PolymerElement, html } from '@polymer/polymer/polymer-element.js';
+import { LitElement, html, css } from "lit";
+import "@polymer/polymer/lib/elements/custom-style.js";
 import './shared-styles.js';
 
-class MyView3 extends PolymerElement {
-  static get template() {
-    return html`
-      <style include="shared-styles">
+class MyView3 extends LitElement {
+  static get styles() {
+    let styles = [];
+    styles.push(
+      css`
         :host {
           display: block;
-
           padding: 10px;
         }
-      </style>
+      `
+    );
+    return styles;
+  }
+  static get properties() {
+    return {
+      data: Object
+    }
+  }
+  //fetch data
+  async resolveData() {
+    const response = await fetch('https://jsonplaceholder.typicode.com/todos?_limit=5');
+    const empData = await response.json();
+    // let data = null;
+
+    this.data = empData;
+    return this.data;
+  }
+
+  async connectedCallback() {
+    super.connectedCallback()
+    //api call
+    this.data = await this.resolveData()
+    //print in console
+    console.log(this.data)
+  }
+  constructor() {
+    super()
+    this.data = {}
+  }
+  render() {
+    return html`
+    <custom-style>
+    <style include="shared-styles"></style>
+  </custom-style>
 
       <div class="card">
         <div class="circle">3</div>
